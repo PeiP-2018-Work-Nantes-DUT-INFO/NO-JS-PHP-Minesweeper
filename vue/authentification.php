1aae2c0ac8b7d20a80d875a24957158ed5e83e23
Eliott Dubois <eliott.dubois@gmail.com>
2019-12-10 00:04:28 +0100

    Add view (html, css)

--- vue/authentification.php
+++ vue/authentification.php
@@ -5,22 +5,56 @@ class Authentification
     public function demandePseudo($err)
     {
         header("Content-type: text/html; charset=utf-8"); ?>
-<html>
+        <html>
+            <head>
+                <link rel="stylesheet" href="assets/login.css">
+            </head>
+        <body>
 
-<body>
+            <div class="logon">
+                <div class="header">Log On to Windows</div>
+                <div class="winxp-bg"></div>
 
-    <br />
-    <br />
-    <b><?=$err ? $err : ''?></b>
-    <form method="post" action="index.php">
-        Entrer votre pseudo <input type="text" name="pseudo" />
-        </br>
-        </br>
-        <input type="submit" name="soumettre" value="envoyer" />
-    </form>
-    <br />
-    <br />
+                <div class="content">
+                    <form method="post" action="index.php">
+                        <div class="field">
+                            <label for="username">User name:</label>
+                            <input type="text" name="username" id="username">
+                        </div>
+                        <div class="field">
+                            <label for="password">Password:</label>
+                            <input type="password" name="password" id="password">
+                        </div>
+                        <div class="buttons">
+                            <input type="submit" name="submit" id="submit" value="OK">
+                            <input type="reset" name="cancel" value="Cancel">
+                            <button>Options >></button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+            <?php if ($err) {$this->loadError();} ?>
+        </body>
+        </html>
     <?php
+    }
+
+
+    public function loadError() {
+        ?>
+        <html>
+        <body>
+            <div class="error">
+                <div class="header">Logon Message</div>
+                <div class="content">
+                    <img src="#" alt="warning_icon">
+                    <p>The system could not log you on. Make sure your User name is correct, then type your password again. Letters in passwords must be typed using the correct case.</p>
+                </div>
+            </div>
+        </body>
+        </html>
+        <?php
     }
 }
 ?>
