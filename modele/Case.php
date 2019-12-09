2f7319bc75a069f655077401a0ba9194272d53b4
Simon Sassi <simon.sassi@etu.univ-nantes.fr>
2019-12-09 18:29:32 +0100

    Reworked model for minesweeper

--- modele/Case.php
+++ modele/Case.php
@@ -1,15 +1,53 @@
-<?php 
+<?php
 class CaseMetier
 {
+    /**
+     * Si vrai, la case est jouée
+     *
+     * @var boolean
+     */
     public $jouee;
+
+    /**
+     * Donne le nombre de mines adjacentes
+     *
+     * @var int
+     */
     public $nombreMinesAdjancentes;
-    public $x;
-    public $y;
-    public function __construct($jouee, $nombreMinesAdjancentes, $x, $y)
+
+    /**
+     * Si est à vraie, la case est une mine
+     *
+     * @var boolean
+     */
+    public $estMine;
+
+
+    public function __construct($jouee, $nombreMinesAdjancentes, $estMine)
     {
         $this->jouee = $jouee;
         $this->nombreMinesAdjancentes = $nombreMinesAdjancentes;
-        $this->x = $x;
-        $this->y = $y;
+        $this->estMine = $estMine;
+    }
+
+    public function jouer()
+    {
+        if ($this->estMine) {
+            $this->jouee = true;
+        }
+    }
+
+    public function getMinesAdjacentes() : int
+    {
+        return $this->nombreMinesAdjancentes;
+    }
+
+    public function estUneMine() : bool
+    {
+        return $this->estMine;
+    }
+
+    public function estJouee() : bool {
+        return $this->jouee;
     }
 }
