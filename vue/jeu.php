41c69fe7f8ed39c4dde7b3e70c2bc25fdb88cfed
Eliott Dubois <eliott.dubois@gmail.com>
2019-12-11 00:13:17 +0100

    Add view jeu.php

--- vue/jeu.php
+++ vue/jeu.php
@@ -3,19 +3,45 @@
 class Jeu
 {
 
-    public function interfaceMessage($pseudo, $messages)
+    public function view($gameState)
     {
-        header("Content-type: text/html; charset=utf-8");
         ?>
-
-        <html>
-        <body>
-            <?php foreach ($messages as $msg): ?>
-            <p><b><?=$msg->pseudo?></b>: <?=$msg->message?> </p>
-            <?php endforeach;?>
-        </body>
-        </html>
-    <?php
+            <html>
+                <div class="content">
+                    <div class="game_window">
+                        <div class="head box-shadow">
+                            <div class="n-decouvert">
+                                <a href="#" draggable="false"></a>
+                            </div>
+                        </div>
+                        <div class="game box-shadow">
+                            <table>
+                                <?php
+                                    $etatCases = $gameState->obtenirEtatJeu();
+                                    for ($ligne=0; $ligne < NBR_LIGNES; $ligne++) { 
+                                        ?>
+                                            <tr>
+                                                <?php
+                                                    for ($colonne=0; $colonne < NBR_COLONNES; $colonne++) {
+                                                        $case = $etatCases[$colonne][$ligne];
+                                                        $nb_mines = $case->getMinesAdjacentes();
+                                                        if ($nb_mines) {
+                                                            ?><td class="decouvert mine-<?= $nb_mines ?>"><?= $nb_mines ?></td><?php
+                                                        } else {
+                                                            ?><td class="n-decouvert"><a href="index.php?x=<?= $colonne ?>&y=<?= $ligne ?>" draggable="false"></a></td><?php
+                                                        }
+                                                    }
+                                                ?>
+                                            </tr>
+                                        <?php
+                                    }
+                                ?>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </html>
+        <?php
     }
 }
 ?>
