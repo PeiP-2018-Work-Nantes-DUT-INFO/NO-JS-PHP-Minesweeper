0e127053ce922de3e8f0c0140772870bec7c8163
mxb <mxb@mxb.com>
2019-12-11 21:18:00 +0100

    Reformated auth

--- controleur/controleurAuthentification.php
+++ controleur/controleurAuthentification.php
@@ -2,39 +2,39 @@
 require_once PATH_VUE."/authentification.php";
 require_once PATH_MODELE."/modele.php";
 
-class ControleurAuthentification{
+class ControleurAuthentification
+{
+    private $vue;
+    private $modele;
 
-	private $vue;
-	private $modele;
+    public function __construct()
+    {
+        $this->vue = new Authentification();
+        $this->modele = new Modele();
+    }
 
-	function __construct(){
-		$this->vue = new Authentification();
-		$this->modele = new Modele();
-	}
+    public function accueil()
+    {
+        $this->vue->connexion(null);
+    }
 
-	function accueil(){
-		$this->vue->connexion(null);
-	}
+    public function login($login, $password)
+    {
+        if ($this->modele->exists($login)) {
+            if ($this->modele->verifierPassword($login, $password)) {
+                $_SESSION['pseudo']=$login;
+                header('Location: index.php', false, 301);
+            } else {
+                $this->vue->connexion('Le mot de passe est incorrect');
+            }
+        } else {
+            $this->vue->connexion('Le pseudo n\'est pas dans la base de données');
+        }
+    }
 
-	function login($login, $password) {
-		if($this->modele->exists($login)) {
-			if ($this->modele->verifierPassword($login, $password)) {
-				$_SESSION['pseudo']=$login;
-				header('Location: index.php', false, 301);
-			} else {
-				$this->vue->connexion('Le mot de passe est incorrect');
-			}
-		} else {
-			$this->vue->connexion('Le pseudo n\'est pas dans la base de données');
-		}
-	}
-
-	function logout() {
-		session_destroy();
-		header('Location: index.php', false, 301);
-	}
-
-
-
-
+    public function logout()
+    {
+        session_destroy();
+        header('Location: index.php', false, 301);
+    }
 }
