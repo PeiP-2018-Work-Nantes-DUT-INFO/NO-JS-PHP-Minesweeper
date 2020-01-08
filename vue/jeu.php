22578cb7e379b7d230dec8d65f125ef7b6d7c247
Eliott Dubois <eliott.dubois@gmail.com>
2020-01-08 22:29:10 +0100

    Fixes - images are no longer draggable - time is utc +1

--- vue/jeu.php
+++ vue/jeu.php
@@ -200,18 +200,23 @@ class VueJeu
             <div class="winbar">
                 <div class="start_stuff">
                     <div class="start">
-                        <img class="logo" src="assets/img/startButton.png" alt="startButton">
+                        <img class="logo" src="assets/img/startButton.png" alt="startButton" draggable="false">
                     </div>
                     <div class="process_gameMS">
-                        <img class="logo" src="assets/img/mine_logo.png" alt="mine_logo">
+                        <img class="logo" src="assets/img/mine_logo.png" alt="mine_logo" draggable="false">
                         <div class="title">Minesweeper</div>
                     </div>
                 </div>
                 <div class="end_stuff">
-                    <img src="assets/img/sound.png" alt="speaker">
-                    <img src="assets/img/usb.png" alt="usb">
-                    <img src="assets/img/shield.png" alt="shield">
-                    <div class="date"><?= date('h:i A'); ?></div>
+                    <img src="assets/img/sound.png" alt="speaker" draggable="false">
+                    <img src="assets/img/usb.png" alt="usb" draggable="false">
+                    <img src="assets/img/shield.png" alt="shield" draggable="false">
+                    <div class="date">
+                    <?php
+                        $utc1 = 3600;
+                        echo gmdate('g:i A', time()+($utc1));
+                        ?>
+                    </div>
                 </div>
             </div>
         <?php
